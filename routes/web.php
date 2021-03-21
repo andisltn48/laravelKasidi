8a025dde8ca3cd48507e3b7269ea1d880e65a40a
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2021-03-21 20:54:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('vIndex');
+});
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'check']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::get('/home', function(){
+    return view('vHome');
 });
 
 Route::get('/dashboard', function () {
