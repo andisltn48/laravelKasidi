01cc800feaafa47a0b519d2f2f81c8828b7e3dd3
andisltn48 <andiaril186@gmail.com>
2021-03-29 09:27:25 +0800

    Update vUserProfile.blade.php
    -

--- resources/views/vUserProfile.blade.php
+++ resources/views/vUserProfile.blade.php
@@ -2,40 +2,48 @@
 
 @section('content')
     <div class="">
-        <div class="container-fluid mt-4 position-fixed">
+        <div class="container-fluid mt-5 pt-5">
+            @if (session('pesan'))
+                <div class="alert alert-success alert-dismissble">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true"><i
+                            class="fas fa-times"></i></button>
+                    <h4><i class="icon fa fa-check"></i>Success!</h4>
+                    {{ session('pesan') }}.
+                </div>
+            @endif
             @foreach ($user as $data)
                 <div class="row">
                     <div class="col-auto m-5">
-                        <img class="rounded" src="{{url('images/fotoProfile/'.$data->foto)}}" alt="">
+                        <img class="rounded" src="{{ url('images/fotoProfile/' . $data->foto) }}" alt="">
                     </div>
                     <div class="col mt-5 ms-2">
                         <div class="nama">
                             <label class="fw-bold" for="">Nama</label>
-                            <p>{{$data->nama}}</p>
+                            <p>{{ $data->nama }}</p>
                         </div>
                         <div class="nim">
                             <label class="fw-bold" for="">NIM</label>
-                            <p>{{$data->nim}}</p>
+                            <p>{{ $data->nim }}</p>
                         </div>
                         <div class="email">
                             <label class="fw-bold" for="">Email</label>
-                            <p>{{$data->email}}</p>
+                            <p>{{ $data->email }}</p>
                         </div>
 
                         <div class="jenisKelamin">
                             <label class="fw-bold" for="">Jenis Kelamin</label>
-                            <p>{{$data->jenis_kelamin}}</p>
+                            <p>{{ $data->jenis_kelamin }}</p>
                         </div>
                         <div class="prodi">
                             <label class="fw-bold" for="">Prodi</label>
-                            <p>{{$data->prodi}}</p>
+                            <p>{{ $data->prodi }}</p>
                         </div>
                         <div class="jurusan">
                             <label class="fw-bold" for="">Jurusan</label>
-                            <p>{{$data->jurusan}}</p>
+                            <p>{{ $data->jurusan }}</p>
                         </div>
-                        <div class="text-center" >
-                            <a href="{{route('profile.editProfile.create',$data->user_id)}}">
+                        <div class="text-center">
+                            <a href="{{ route('profile.editProfile.create', $data->user_id) }}">
                                 <button type="submit" class="btn btn-warning  align-item-bottom">Edit Data</button>
                             </a>
                         </div>
@@ -45,4 +53,3 @@
         </div>
     </div>
 @endsection
-
