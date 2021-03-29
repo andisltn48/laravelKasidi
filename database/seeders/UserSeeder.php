f8fd3383a77ff3346cdf76363b357dce3b67036d
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-03-29 11:03:07 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Database\Seeders;
+
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -16,17 +18,17 @@ class UserSeeder extends Seeder
     {
         User::insert([
             'id' => 1,
+            'name' => 'member',
+            'email' => 'member@gmail.com',
+            'password' => hash::make('member'),
+            'role' => 'member',
+        ]);
+        User::insert([
+            'id' => 2,
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => hash::make('admin'),
             'role' => 'admin',
-        ]);
-        User::insert([
-                        'id' => 2,
-                        'name' => 'member',
-                        'email' => 'member@gmail.com',
-                        'password' => hash::make('member'),
-                        'role' => 'member',
         ]);
     }
 }
